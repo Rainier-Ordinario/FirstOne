2acdb705fbf0ce8e969e1b76a98653bd1719afc6
Rainier Ordinario <rainmordi@gmail.com>
2025-02-12 23:49:13 -0800

    transpose a matrix

--- main123.py
+++ main123.py
@@ -4,11 +4,22 @@
 
 
 
-#Calculate the distance of a vector ||v|| = sqrt(v1^2 + v2^2)
+# Calculate the distance of a vector ||v|| = sqrt(v1^2 + v2^2)
 
 vector1 = int(input("Enter the vector1 in R^2 "))
 vector2 = int(input("Enter the vector2 in R^2 "))
 
 print(((vector1**2) + (vector2**2))**1/2)
 
+# Transpoing a matrix
 
+matrix = [1, 2, 3]
+tranposed_matrix = []
+def transpose():
+    
+    for num in matrix:
+        tranposed_matrix.append(num)
+    
+print(tranposed_matrix)
+
+# Fix this 
