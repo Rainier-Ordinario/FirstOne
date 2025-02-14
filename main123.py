7cbf8a9a4207ab3a3061dd60a177c7a171807e80
Rainier Ordinario <rainmordi@gmail.com>
2025-02-13 23:55:43 -0800

    call the function

--- main123.py
+++ main123.py
@@ -6,20 +6,26 @@
 
 # Calculate the distance of a vector ||v|| = sqrt(v1^2 + v2^2)
 
-vector1 = int(input("Enter the vector1 in R^2 "))
-vector2 = int(input("Enter the vector2 in R^2 "))
+# vector1 = int(input("Enter the vector1 in R^2 "))
+# vector2 = int(input("Enter the vector2 in R^2 "))
 
-print(((vector1**2) + (vector2**2))**1/2)
+# print(((vector1**2) + (vector2**2))**1/2)
 
 # Transpoing a matrix
 
-matrix = [1, 2, 3]
-tranposed_matrix = []
+matrix = [[1, 2],[3, 4]]
+
 def transpose():
-    
+    transposed_matrix = []
     for num in matrix:
-        tranposed_matrix.append(num)
+        transposed_matrix.append(num)
+        
+    print(transposed_matrix)
+
+transpose()
+       
+
     
-print(tranposed_matrix)
+
 
 # Fix this 
